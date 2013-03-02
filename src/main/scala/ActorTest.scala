2ed55584a34fa690adcc58a0b0b5d47457a9ad86
flysheep1980 <flysheep1980@gmail.com>
2013-03-02 11:56:22 +0900

    .

--- src/main/scala/ActorTest.scala
+++ src/main/scala/ActorTest.scala
@@ -2,26 +2,39 @@ object ActorTest {
 
   import scala.actors.Actor._
   import java.util.Date
+  import scala.util.control.Exception.allCatch
 
   private lazy val tester = actor {
     loop {
       react {
         case message: String => {
           println("[%s][%s]received message and waiting...".format(new Date().getTime, message))
+          val result = allCatch.either {
+            if (message.indexOf("3") < 0) throw new RuntimeException(message) else message
+          }
           println("[%s][%s]finish to wait thread.".format(new Date().getTime, message))
-          reply(message)
+          reply(result)
         }
+        case 0 => exit()
       }
     }
   }
 
   def execute = {
     val futures = (1 to 10).map { index =>
-      tester !! "message%d".format(index)
+      val message = "message%d".format(index)
+      val future = tester !! (message, new PartialFunction[Any, Either[Throwable, String]] {
+        def apply(v1: Any): Either[Throwable, String] = v1.asInstanceOf[Either[Throwable, String]]
+        def isDefinedAt(x: Any): Boolean = true
+      })
+      (message, future)
     }
-    val results = futures.map { future =>
-      future.apply().asInstanceOf[String]
-    }aa
+    val results = futures.map { case (message, future) =>
+      future.apply()
+    }
     results.foreach(println)
+
+    // exit
+    tester ! 0
   }
 }
